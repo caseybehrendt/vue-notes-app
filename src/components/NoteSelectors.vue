056cda294914002ecb2c580d181189d872cb4c51
CASEY BEHRENDT <behrendtc@gmail.com>
2022-08-03 20:19:21 -0600

    create note app

---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,25 @@
+<script>
+import NoteSelector from "./NoteSelector";
+
+export default {
+  components: {
+    NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "This is a first note", timestamp: Date.now() },
+        { id: 2, body: "This is a second note", timestamp: Date.now() },
+        { id: 3, body: "This is a third note", timestamp: Date.now() },
+        { id: 4, body: "This is a fourth note", timestamp: Date.now() },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="note-selectors">
+    <NoteSelector v-for="note in notes" v-bind:key="note.id" v-bind:note="note" />
+  </div>
+</template>
